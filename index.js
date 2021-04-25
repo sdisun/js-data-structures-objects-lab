b1431305d9a65ee36d38f8d26725a42f0c121e1f
Neil <zooming.in@hotmail.com>
2021-04-26 00:12:56 +1000

    updates

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const driver = {
+    name: "Sam",
+    stressAddress: "11 Broadway"
+};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newObj = {...driver};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key){
+    const newObj2 = {...driver}
+    delete newObj2[key];
+    return newObj2;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key];
+    return driver;
+}
+
